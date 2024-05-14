433316a8743836f4824e49c5ffac75dbe3251ab4
AlexLitvino <litvinov.aleks@gmail.com>
2024-05-14 22:16:59 +0300

    added docstrign to statistics_utils.py

--- calculapiz/statistics_utils.py
+++ calculapiz/statistics_utils.py
@@ -15,6 +15,8 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
+
+This module contains utilities functions to work with statistics
 """
 import csv
 
